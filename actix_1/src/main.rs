5b206d9c74f55ebd66ef0aa8ccb3d47d4825eea4
jgkim <jgkim999@gmail.com>
2026-06-15 22:01:06 +0900

    Reorder imports in main.rs for better readability

--- actix_1/src/main.rs
+++ actix_1/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
